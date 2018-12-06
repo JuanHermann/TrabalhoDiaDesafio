b602534d0c9d8083633f6c525c64287c9f7f76af
Roberta Garcia <robgarcia_@hotmail.com>
2018-12-06 19:09:56 -0200

    Merge branches 'JuanLogin2' and 'master' of https://github.com/JuanHermann/TrabalhoDiaDesafio
    # Conflicts:
    #	.idea/workspace.xml
    #	TrabalhoDiaDesafio/app/src/main/AndroidManifest.xml
    #	TrabalhoDiaDesafio/app/src/main/java/com/example/juan/trabalhodiadesafio/PrincipalActivity.java
    #	TrabalhoDiaDesafio/app/src/main/java/com/example/juan/trabalhodiadesafio/view/LoginActivity.java
    #	TrabalhoDiaDesafio/app/src/main/res/layout/activity_principal.xml

---
+++ TrabalhoDiaDesafio/app/src/main/java/com/example/juan/trabalhodiadesafio/model/Grupo.java
@@ -0,0 +1,24 @@
+package com.example.juan.trabalhodiadesafio.model;
+
+public class Grupo {
+
+    private Long idgrupo;
+    private String descricao;
+
+    public Long getIdgrupo() {
+        return idgrupo;
+    }
+
+    public void setIdgrupo(Long idgrupo) {
+        this.idgrupo = idgrupo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}

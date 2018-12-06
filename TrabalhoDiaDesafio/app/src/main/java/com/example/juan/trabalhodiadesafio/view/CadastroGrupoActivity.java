4597efc6b2dc51e175cce39f6bdd17631e17fb50
Roberta Garcia <robgarcia_@hotmail.com>
2018-12-06 19:28:17 -0200

    Merge branches 'JuanLogin2' and 'master' of https://github.com/JuanHermann/TrabalhoDiaDesafio
    # Conflicts:
    #	.idea/workspace.xml
    #	TrabalhoDiaDesafio/app/src/main/AndroidManifest.xml
    #	TrabalhoDiaDesafio/app/src/main/java/com/example/juan/trabalhodiadesafio/PrincipalActivity.java
    #	TrabalhoDiaDesafio/app/src/main/java/com/example/juan/trabalhodiadesafio/view/LoginActivity.java
    #	TrabalhoDiaDesafio/app/src/main/res/layout/activity_principal.xml

--- TrabalhoDiaDesafio/app/src/main/java/com/example/juan/trabalhodiadesafio/view/CadastroGrupoActivity.java
+++ TrabalhoDiaDesafio/app/src/main/java/com/example/juan/trabalhodiadesafio/view/CadastroGrupoActivity.java
@@ -1,15 +1,21 @@
 package com.example.juan.trabalhodiadesafio.view;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.juan.trabalhodiadesafio.R;
 import com.example.juan.trabalhodiadesafio.model.Grupo;
+import com.example.juan.trabalhodiadesafio.service.GrupoService;
+import com.example.juan.trabalhodiadesafio.service.ServiceGenerator;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import retrofit2.Call;
 
 @EActivity(R.layout.activity_cadastro_grupo)
 public class CadastroGrupoActivity extends AppCompatActivity {
@@ -27,6 +33,33 @@ public class CadastroGrupoActivity extends AppCompatActivity {
     void clickSalvar() {
         Grupo grupo = new Grupo();
 
-        grupo.setDescricao(edtDescricao.getText().toString());
+        if (!edtDescricao.getText().toString().isEmpty()) {
+            grupo.setDescricao(edtDescricao.getText().toString());
+
+            if (android.os.Build.VERSION.SDK_INT > 9) {
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+
+            try {
+                GrupoService grupoService = ServiceGenerator.createService(GrupoService.class);
+
+                Call<Void> call;
+
+                call = grupoService.insert(grupo);
+
+                call.execute().body();
+
+                Toast.makeText(this, "Registro salvo com sucesso!", Toast.LENGTH_SHORT).show();
+
+                finish();
+            } catch (Exception ex) {
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+                ex.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "O campo DESCRIÇÃO é de preenchimento obrigatório.", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
